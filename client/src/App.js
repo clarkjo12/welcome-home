75f18a25a6f4059f51b707f3a60066adaf241fdb
Jon <clarkjo12@gmail.com>
2020-08-26 14:57:49 -0400

    intial push

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="landing">
+        <div className="cabin-background">
+          <p className="welcome">Welcome Home,</p>
+        </div>
+      </header>
+
+      <div className="second-base">hello</div>
+    </div>
+  );
+}
+
+export default App;
